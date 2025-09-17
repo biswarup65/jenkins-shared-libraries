76d802a0e264e01b1d14b28e7beac09750c7aa17
Biswarup Basak <153805988+biswarup65@users.noreply.github.com>
2025-09-17 18:45:47 +0530

    Update docker_deploy.groovy

--- vars/docker_deploy.groovy
+++ vars/docker_deploy.groovy
@@ -1,5 +1,4 @@
 def call(){
-  sh " docker-compose down"
-  sh " docker-compose up -d --build"
-  sh " Application deployed successfully and app run on port 8000"
+  sh "docker-compose down"
+  sh "docker-compose up -d --build"
 }
